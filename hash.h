5bc0cc557851969a542aa43aca22d905a6ecb8a3
kevinhofstetter3 <69937200+kevinhofstetter3@users.noreply.github.com>
2021-10-30 23:01:12 -0400

    Added comments

--- hash.h
+++ hash.h
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 #define DIGEST_SIZE 5 //The size of SHA_40 hash in bytes
-#define BLOCK_SIZE sizeof(struct Block) // Block size in bytes
-#define BYTE unsigned char // if you want use BYTE in place of unsigned char
+#define BLOCK_SIZE sizeof(struct Block) //Block size in bytes
+#define BYTE unsigned char //Potential replacement for 'unsigned char' with BYTE
 
 //SHA_40 Digest
 struct Digest{
     BYTE hash0, hash1, hash2, hash3, hash4;
 };
 
-unsigned char* SHA_40(const unsigned char* data, size_t size); // SHA_40 hash function 
-int digest_equal(struct Digest *digest1, struct Digest *digest2); //return 1 (true) or 0 (false)
+unsigned char* SHA_40(const unsigned char* data, size_t size); //SHA_40 hash function
+int digest_equal(struct Digest *digest1, struct Digest *digest2); //return 1 if equal, 0 if otherwise
